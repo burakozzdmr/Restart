fab86f8348920d0f1afb76bff013cc642f7e2f8e
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-03-05 21:00:23 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Burak Özdemir on 5.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
